1bbd6784f087ec6b7d3d4af51b0cfa7f33931061
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-02-28 18:48:23 -0300

    Update scenario.py

--- sintetizador/services/synthesis/scenario.py
+++ sintetizador/services/synthesis/scenario.py
@@ -1810,6 +1810,7 @@ class ScenarioSynthetizer:
         print("num_iteracoes: ", num_iteracoes, " len: ", num_iteracoes)
         print("np.tile(mlts_ordenadas, num_iteracoes): ", np.tile(mlts_ordenadas, num_iteracoes), " len: ", len(np.tile(mlts_ordenadas, num_iteracoes)))
         print("df: ", df)
+        print("df_filtro: ", df.loc[(df["nome_usina"] == "14 DE JULHO") & (df["estagio"] == 1)  ])
         df["mlt"] = np.tile(mlts_ordenadas, num_iteracoes)
         print("df[mlt] : " , df["mlt"] )
         df["valorMlt"] = df["valor"] / df["mlt"]
