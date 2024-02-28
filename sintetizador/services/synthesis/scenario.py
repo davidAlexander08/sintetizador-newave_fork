2be784c6137f518ca447fd1445326054e041adf0
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-02-28 14:33:41 -0300

    Update scenario.py

--- sintetizador/services/synthesis/scenario.py
+++ sintetizador/services/synthesis/scenario.py
@@ -505,7 +505,7 @@ class ScenarioSynthetizer:
         print("configuracao: ", np.concatenate((np.array([1] * (12 + mes_inicio - 1)), cfgs)), " len: ", len(np.concatenate((np.array([1] * (12 + mes_inicio - 1)), cfgs))))
         print("mes: ", [d.month for d in datas], " len: ", len([d.month for d in datas]))
 
-        print("d.month: ", d.month, " datas: ", datas)
+        print("d.month: ", datas.month, " datas: ", datas)
         
         df_mlt = pd.DataFrame(data={"estagio": list(range(-(12 + mes_inicio - 2), len(cfgs) + 1)),
                                     "configuracao": np.concatenate((np.array([1] * (12 + mes_inicio - 1)), cfgs)),
