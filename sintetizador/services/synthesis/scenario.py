d34b8c022b5ecbce4b761b23ef656324d02d41ae
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-01 14:03:04 -0300

    Update scenario.py

--- sintetizador/services/synthesis/scenario.py
+++ sintetizador/services/synthesis/scenario.py
@@ -495,6 +495,7 @@ class ScenarioSynthetizer:
             arq_engnat.series, pd.DataFrame, "séries de energia"
         )
         cfgs = configuracoes["valor"].to_numpy().flatten()[mes_inicio - 1 :]
+        print("cfgs: ", cfgs)
         datas = pd.date_range(
             datetime(year=ano_inicio - 1, month=1, day=1),
             #datetime(year=ano_inicio + anos_estudo + anos_pos_estudo - 1, month=12, day=1),
