91d0e329f64871c1578d8743d6988e7df5986c4a
David Alexander <142263303+davidAlexander08@users.noreply.github.com>
2024-03-21 10:04:29 -0300

    Update files.py

--- sintetizador/adapters/repository/files.py
+++ sintetizador/adapters/repository/files.py
@@ -1427,8 +1427,8 @@ class RawFilesRepository(AbstractFilesRepository):
         rees = self._validate_data(arq_ree.rees, pd.DataFrame)
         mes_fim_hib = rees["mes_fim_individualizado"].iloc[0]
         ano_fim_hib = rees["ano_fim_individualizado"].iloc[0]
-        if ((type(mes_fim_hib) == int or float) and 
-            (type(ano_fim_hib) == int or float) ):
+        if (  isinstance(mes_fim_hib,(int, float) and  
+                         isinstance(ano_fim_hib,(int, float)):
             
         #if (mes_fim_hib is not None and ano_fim_hib is not None):
             data_inicio_estudo = datetime(
